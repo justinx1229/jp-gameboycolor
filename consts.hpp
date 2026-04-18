1ee64f16baea6a23c2d8f5b2cea35c8998a3aeae
omeganotation <patcdeng@gmail.com>
2026-04-18 15:23:24 -0500

    mem?

--- consts.hpp
+++ consts.hpp
@@ -1,7 +1,7 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 
-const uint32_t WIDTH = 320;
-const uint32_t HEIGHT = 288;
+const uint32_t WIDTH = 160;
+const uint32_t HEIGHT = 144;
 
 const uint32_t DELAY = 5000;
---
+++ memory.cpp
@@ -0,0 +1,9 @@
+#include "memory.hpp"
+
+uint8_t read_byte(uint16_t address) {
+    return 0;
+}
+
+void write_byte(uint16_t address, uint8_t byte) {
+
+}
