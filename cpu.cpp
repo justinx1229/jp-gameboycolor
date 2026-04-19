96db0bf19867fd2d7dc2a58fffd377abec42e063
omeganotation <patcdeng@gmail.com>
2026-04-19 00:49:10 -0500

    lc

--- cpu.cpp
+++ cpu.cpp
@@ -292,8 +292,10 @@ void run00(uint8_t byte) {
             break;
         case 6:
             set_r8((byte >> 3) & LO_3, next8());
+            break;
         case 14:
             set_r8((byte >> 3) & LO_3, next8());
+            break;
         case 7: {
             uint8_t hi_4 = byte >> 4;
             switch (hi_4) {
@@ -304,7 +306,7 @@ void run00(uint8_t byte) {
                     break;
                 }
                 case 1: {
-                    uint8_t tc = flags[3];
+                    uint8_t tc = flags[0];
                     flags[0] = a & 128;
                     flags[1] = 0; flags[2] = 0; flags[3] = 0;
                     a = ((a & LO_7) << 1) | tc;
@@ -342,8 +344,41 @@ void run00(uint8_t byte) {
                     exit(1);
                     break;
             }
+            break;
         }
         case 15:
+            uint8_t hi_4 = byte >> 4;
+            switch (hi_4) {
+                case 0: {
+                    flags[0] = a & 1;
+                    flags[1] = 0; flags[2] = 0; flags[3] = 0;
+                    a = ((a & 1) << 7) | (a >> 1);
+                    break;
+                }
+                case 1: {
+                    uint8_t tc = flags[0];
+                    flags[0] = a & 1;
+                    flags[1] = 0; flags[2] = 0; flags[3] = 0;
+                    a = (tc << 7) | (a >> 1);
+                    break;
+                }
+                case 2: {
+                    a = ~a;
+                    flags[2] = 1;
+                    flags[1] = 1;
+                    break;
+                }
+                case 3: {
+                    flags[2] = 0;
+                    flags[1] = 0;
+                    flags[0] = !flags[0];
+                    break;
+                }
+                default: 
+                    std::cerr << "Invalid instruction " << std::bitset<8>(byte).to_string() << " at pc " << pc << "\n";
+                    exit(1);
+                    break;
+            }
             break;
     }
 }
