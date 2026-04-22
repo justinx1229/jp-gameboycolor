d17359635c53c9b4f04544fb84175afef329822c
Justin Xu <justin.xu.1229@gmail.com>
2026-04-21 22:24:51 -0500

    uses surface instead of texture in display.cpp

--- display.cpp
+++ display.cpp
@@ -1,27 +1,14 @@
 #include "display.h"
 
-SDL_Renderer *renderer = nullptr;
-SDL_Texture *texture = nullptr;
+SDL_Surface *surface = nullptr;
+SDL_Window *g_window = nullptr;
 
 bool init_display(SDL_Window *window) {
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    if (!renderer) {
-        std::cerr << "Renderer not there: " << SDL_GetError() << "\n";
-        return false;
-    }
+    g_window = window;
+    surface = SDL_GetWindowSurface(window);
 
-    texture = SDL_CreateTexture(
-        renderer,
-        SDL_PIXELFORMAT_ARGB8888,
-        SDL_TEXTUREACCESS_STREAMING,
-        WIDTH,
-        HEIGHT
-    );
-
-    if (!texture) {
-        std::cerr << "Texture not there: " << SDL_GetError() << "\n";
-        SDL_DestroyRenderer(renderer);
-        renderer = nullptr;
+    if (!surface) {
+        std::cerr << "Surface not there: " << SDL_GetError() << "\n";
         return false;
     }
 
@@ -29,24 +16,12 @@ bool init_display(SDL_Window *window) {
 }
 
 void render_display(const uint32_t frame_buffer[HEIGHT][WIDTH]) {
-    if (!renderer || !texture) {
-        return;
-    }
+    if (!surface) return;
 
-    SDL_UpdateTexture(texture, nullptr, frame_buffer, WIDTH * sizeof(uint32_t));
-    SDL_RenderClear(renderer);
-    SDL_RenderCopy(renderer, texture, nullptr, nullptr);
-    SDL_RenderPresent(renderer);
+    memcpy(surface->pixels, frame_buffer, WIDTH * HEIGHT * sizeof(uint32_t));
+    SDL_UpdateWindowSurface(g_window);
 }
 
 void destroy_display() {
-    if (texture) {
-        SDL_DestroyTexture(texture);
-        texture = nullptr;
-    }
-
-    if (renderer) {
-        SDL_DestroyRenderer(renderer);
-        renderer = nullptr;
-    }
+    surface = nullptr;
 }
